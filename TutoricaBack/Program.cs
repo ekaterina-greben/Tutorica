20f4db39b0e9968f575505c27ed29abbf64d712f
Ekaterina Greben <katenka26@gmail.com>
2022-10-27 21:02:34 +0000

    Program.cs has been modified, routes and requests updated

--- TutoricaBack/Program.cs
+++ TutoricaBack/Program.cs
@@ -1,7 +1,12 @@
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
+using System.Collections.Specialized;
 
-Console.WriteLine("Hello, everyone! Version 0.0.2");
+string formatLogString(string logevent) {
+    return "[" + DateTime.Now + "] " + logevent;
+}
+
+Console.WriteLine("API Server Version 0.0.3");
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,65 +32,93 @@ string connectionString = "Server=" + config.GetValue<string>("dbhost") + ";" +
                             "User Id=" + config.GetValue<string>("dbuser") + ";" +
                             "Password=" + config.GetValue<string>("dbpass") + ";";
 
-System.Console.WriteLine(connectionString);
+app.Logger.LogDebug(formatLogString(connectionString));
 
 var connection = new SqlConnection(connectionString);
 connection.Open();
 
 
-app.MapGet("/enterprises", () =>
+app.MapGet("/enterprises", (HttpRequest request, HttpResponse response) =>
 {
-    string sql = "SELECT * FROM [dbo].[items]";
+    app.Logger.LogInformation(formatLogString("GET /enterprises " + "from " + request.Host));
+    string sql = @"SELECT id, firstName, lastName, enterpriseName, phoneNumber, email, entDescription, age, a_t_USD, course, a_t_UAH * FROM [dbo].[items]";
     using var cmd = new SqlCommand(sql, connection);
 
     using SqlDataReader rdr = cmd.ExecuteReader();
 
-    string resultString = "";
+    List<ListDictionary> answer = new List<ListDictionary>();
 
-    while (rdr.Read())
+    while (rdr.Read()) 
     {
-        resultString += rdr.GetInt32(0).ToString() + ";" +
-                        rdr.GetString(1) + ";" + 
-                        rdr.GetString(2) + "\n";
+
+        ListDictionary list = new ListDictionary();
+        list.Add("id", rdr.GetInt32(0).ToString());
+        list.Add("firstName", rdr.GetString(1));
+        list.Add("lastName", rdr.GetString(2));
+        list.Add("phoneNumber", rdr.GetString(3));
+        list.Add("email", rdr.GetString(4));
+        list.Add("entDescription", rdr.GetString(5));
+        list.Add("age", rdr.GetInt32(6).ToString());
+        list.Add("a_t_USD", rdr.GetDecimal(7).ToString());
+        list.Add("course", rdr.GetDecimal(8).ToString());
+        list.Add("a_t_UAH", rdr.GetDecimal(9).ToString());
+
+        answer.Add(list);
 
     }
     
-    return resultString;
+    response.Headers.AccessControlAllowOrigin = "*";
+    response.StatusCode = 200;
 
-
+    return answer;
 });
 
-app.MapGet("/getcourse", () =>
+app.MapGet("/getcourse", (HttpRequest request, HttpResponse response) =>
 {
-    string sql = "SELECT * FROM [dbo].[items]";
+    app.Logger.LogWarning(formatLogString("GET /getcourse " + "from " + request.Host));
+    app.Logger.LogInformation(formatLogString("GET /getcourse " + "from " + request.Host));
+    string sql = @"SELECT date, currency, course * FROM [dbo].[items]";
     using var cmd = new SqlCommand(sql, connection);
 
     using SqlDataReader rdr = cmd.ExecuteReader();
 
-    string resultString = "";
+    List<ListDictionary> answer = new List<ListDictionary>();
 
     while (rdr.Read())
     {
-        resultString += rdr.GetInt32(0).ToString() + ";" +
-                        rdr.GetString(1) + ";" + 
-                        rdr.GetString(2) + "\n";
+
+        ListDictionary list = new ListDictionary();
+        list.Add("date", rdr.GetInt32(0).ToString());
+        list.Add("currency", rdr.GetString(1));
+        list.Add("course", rdr.GetFloat(2));
+        
+        answer.Add(list);
 
     }
-    
-    return resultString;
 
+    response.Headers.AccessControlAllowOrigin = "*";
+    response.StatusCode = 200;
 
+    return answer;
 });
 
-app.MapPost("/advertisement", () =>
+
+app.MapPost("/advertisement", (HttpRequest request, HttpResponse response) =>
 {
+    app.Logger.LogDebug(formatLogString("POST /advertisement " + "from " + request.Host));
+    response.Headers.AccessControlAllowOrigin = "*";
+    response.StatusCode = 201;
     return "Method is under contstruction";
 });
 
 app.Run();
 
-app.MapPost("/feedback", () =>
+app.MapPost("/feedback", (HttpRequest request, HttpResponse response) =>
 {
+    app.Logger.LogCritical(formatLogString("POST /feedback " + "from " + request.Host));
+    app.Logger.LogDebug(formatLogString("POST /feedback " + "from " + request.Host));
+    response.Headers.AccessControlAllowOrigin = "*";
+    response.StatusCode = 201;
     return "Method is under contstruction";
 });
 
