f831b7e1944afb41b8b6894841c9828c23e84b15
Ekaterina Greben <katenka26@gmail.com>
2022-11-09 13:52:50 +0000

    Front part has been modified acc. to 8_Front_Blazor_Programming

---
+++ TutoricaFront/Models/Item.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutoricaFront.Models
+{
+    public class Item
+    {
+        public int id {get; set;}  
+        public string? firstName {get; set;} 
+        public string? lastName {get; set;} 
+        public string? name {get; set;} 
+        public string? phoneNumber {get; set;} 
+        public string? email {get; set;} 
+        public string? description {get; set;} 
+        public string? years {get; set;} 
+        public float investmentUSD {get; set;} 
+        public float course {get; set;} 
+        public float investmentUAH {get; set;} 
+        
+    }
+}
